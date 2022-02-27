3af15d7767db570ff322c8717ea4e5b6f997937d
Nazeh <ar.nazeh@gmail.com>
2022-02-27 11:51:55 +0300

    Convert to executable

--- client.js
+++ client.js
@@ -1,11 +1,7 @@
+#!/usr/bin/env node
 import { slashtagsPayClient } from './index.js';
 
-const slashtag = process.argv[2];
-const amount = process.argv[3];
-const description = process.argv[4];
-
 (async () => {
-  const response = await slashtagsPayClient(slashtag, { amount, description });
-
+  await slashtagsPayClient();
   process.exit();
 })();
